99c395be2a5ff308bd79b7dbdeb8f4f1570233cd
Diogo Dantas <diogo.dantas@live.com>
2022-01-27 07:40:52 -0300

    Adicionando serialVersionID para classe SaldoException.

--- dentalOffice/model/SaldoException.java
+++ dentalOffice/model/SaldoException.java
@@ -1,6 +1,8 @@
 package br.com.dentalOffice.model;
 
 public class SaldoException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
 
 	// CHECKED
 	// public class SaldoException extends Exception
