000608214912d27bc923e5f29928356f3f4579e3
Diogo Dantas <diogo.dantas@live.com>
2022-01-27 06:04:07 -0300

    Inclusão TesteLeitura2 com a classe Scanner do package java.util, para leitura de arquivo csv.

---
+++ dentalOffice/test/TesteLeitura2.java
@@ -0,0 +1,31 @@
+package br.com.dentalOffice.test;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+	public static void main(String[] args) throws Exception {
+
+		Scanner scanner = new Scanner(new File("src/br/com/dentalOffice/files/tabelaBase.csv"));
+
+		while (scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+
+			String tratamento = linhaScanner.next();
+			String dentista = linhaScanner.next();
+			double tabelaBase = linhaScanner.nextDouble();
+
+			System.out.format(new Locale("pt", "BR"), "%s - %s: %07.2f%n", tratamento, dentista, tabelaBase);
+
+			linhaScanner.close();
+		}
+
+		scanner.close();
+	}
+
+}
